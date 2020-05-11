3dbc8afb6d9cf1a87c4e5de400445c9bce65bd0e
Neil Zeng <neilz@synnex.com>
2020-05-12 00:35:04 +0800

    feat: 添加图片比对

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-continue */
 type CompareWay =
-  | "CV_TM_SQDIFF"
-  | "CV_TM_SQDIFF_NORMED"
-  | "CV_TM_CCORR"
-  | "CV_TM_CCORR_NORMED"
-  | "CV_TM_CCOEFF"
-  | "CV_TM_CCOEFF_NORMED";
+  | 'CV_TM_SQDIFF'
+  | 'CV_TM_SQDIFF_NORMED'
+  | 'CV_TM_CCORR'
+  | 'CV_TM_CCORR_NORMED'
+  | 'CV_TM_CCOEFF'
+  | 'CV_TM_CCOEFF_NORMED';
 
 // 差值平方和匹配 CV_TM_SQDIFF
 const cvTmSqDiff = (template, search, tWidth, tHeight, sWidth, sHeight) => {
@@ -45,7 +45,7 @@ const cvTmSqDiffNormed = (
   tWidth,
   tHeight,
   sWidth,
-  sHeight
+  sHeight,
 ) => {
   let minValue = Infinity;
   let x = -1;
@@ -117,7 +117,7 @@ const cvTmCcorrNormed = (
   tWidth,
   tHeight,
   sWidth,
-  sHeight
+  sHeight,
 ) => {
   let maxValue = -Infinity;
   let x = -1;
@@ -201,7 +201,7 @@ const cvTmCcoeffNormed = (
   tWidth,
   tHeight,
   sWidth,
-  sHeight
+  sHeight,
 ) => {
   let maxValue = -Infinity;
   let x = -1;
@@ -277,25 +277,386 @@ export const getTemplatePos = (
   tHeight,
   sWidth,
   sHeight,
-  type?: CompareWay
+  type?: CompareWay,
 ) => {
-  if (type === "CV_TM_SQDIFF" || !type) {
+  if (type === 'CV_TM_SQDIFF' || !type) {
     return cvTmSqDiff(template, search, tWidth, tHeight, sWidth, sHeight);
   }
-  if (type === "CV_TM_SQDIFF_NORMED") {
+  if (type === 'CV_TM_SQDIFF_NORMED') {
     return cvTmSqDiffNormed(template, search, tWidth, tHeight, sWidth, sHeight);
   }
-  if (type === "CV_TM_CCORR") {
+  if (type === 'CV_TM_CCORR') {
     return cvTmCcorr(template, search, tWidth, tHeight, sWidth, sHeight);
   }
-  if (type === "CV_TM_CCORR_NORMED") {
+  if (type === 'CV_TM_CCORR_NORMED') {
     return cvTmCcorrNormed(template, search, tWidth, tHeight, sWidth, sHeight);
   }
-  if (type === "CV_TM_CCOEFF") {
+  if (type === 'CV_TM_CCOEFF') {
     return cvTmCcoeff(template, search, tWidth, tHeight, sWidth, sHeight);
   }
-  if (type === "CV_TM_CCOEFF_NORMED") {
+  if (type === 'CV_TM_CCOEFF_NORMED') {
     return cvTmCcoeffNormed(template, search, tWidth, tHeight, sWidth, sHeight);
   }
   return { x: -1, y: -1 };
+};
+
+/**
+ * 比较两幅图片得到 不同处的坐标
+ * @param source1 ImageData
+ * @param source2 ImageData
+ */
+export const compareImage = (source1: ImageData, source2: ImageData) => {
+  // 去到相同背景
+  const leftDataImage: any[] = [];
+  // const newImageData = new ImageData(source1.width, source1.height);
+  const { width, height } = source1;
+  for (let i = 0; i < height; i++) {
+    leftDataImage.push([]);
+    for (let j = 0; j < width; j++) {
+      const offset = (i * width + j) * 4;
+      const r1 = source1.data[offset];
+      const g1 = source1.data[offset + 1];
+      const b1 = source1.data[offset + 2];
+
+      const r2 = source2.data[offset];
+      const g2 = source2.data[offset + 1];
+      const b2 = source2.data[offset + 2];
+
+      if (r1 === r2 && g1 === g2 && b1 === b2) {
+        // 相同则去掉像素值
+        // 保留成0
+        // leftDataImage[i][j] = 0;
+        // newImageData.data[offset] = 255;
+        // newImageData.data[offset + 1] = 255;
+        // newImageData.data[offset + 2] = 255;
+        // newImageData.data[offset + 3] = 255;
+        leftDataImage[i].push(0);
+      } else {
+        // 不同保留成255
+        // leftDataImage[i][j] = 255;
+        // newImageData.data[offset] = 0;
+        // newImageData.data[offset + 1] = 0;
+        // newImageData.data[offset + 2] = 0;
+        // newImageData.data[offset + 3] = 255;
+        leftDataImage[i].push(255);
+      }
+    }
+  }
+
+  // 构建上下左右遍历不同区域 找出边界范围->坐标大小
+  const foundNode = {};
+  const getBoundaryNodes = (i, j) => {
+    const returnArr: any[] = [];
+
+    let tplI = i;
+    let tplJ = j;
+    for (;;) {
+      if (foundNode[`${tplI}_${tplJ}`]) {
+        break;
+      }
+      const west = j <= 0 ? 0 : leftDataImage[tplI][tplJ - 1] || 0;
+      foundNode[`${tplI}_${tplJ - 1}`] = true;
+      if (west !== 0) {
+        returnArr.push({
+          i: tplI,
+          j: tplJ--,
+        });
+      } else {
+        break;
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (foundNode[`${tplI}_${tplJ}`]) {
+        break;
+      }
+      // foundNode[`${tplI}_${tplJ}`] = true;
+      const east = leftDataImage[tplI][tplJ + 1] || 0;
+      foundNode[`${tplI}_${tplJ + 1}`] = true;
+      if (east !== 0) {
+        returnArr.push({
+          i: tplI,
+          j: tplJ++,
+        });
+      } else {
+        break;
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (foundNode[`${tplI}_${tplJ}`]) {
+        break;
+      }
+      // foundNode[`${tplI}_${tplJ}`] = true;
+      const north = tplI <= 0 ? 0 : leftDataImage[tplI - 1][tplJ] || 0;
+      foundNode[`${tplI - 1}_${tplJ}`] = true;
+      if (north !== 0) {
+        returnArr.push({
+          i: tplI--,
+          j: tplJ,
+        });
+      } else {
+        break;
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (foundNode[`${tplI}_${tplJ}`]) {
+        break;
+      }
+      // foundNode[`${tplI}_${tplJ}`] = true;
+      const north =
+        tplI >= source1.height - 1 ? 0 : leftDataImage[tplI + 1][tplJ] || 0;
+      foundNode[`${tplI + 1}_${tplJ}`] = true;
+      if (north !== 0) {
+        returnArr.push({
+          i: tplI++,
+          j: tplJ,
+        });
+      } else {
+        break;
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (tplI <= 0 || tplJ >= source1.width - 1) {
+        break;
+      } else {
+        if (foundNode[`${tplI}_${tplJ}`]) {
+          break;
+        }
+        // foundNode[`${tplI}_${tplJ}`] = true;
+        const northeast = leftDataImage[tplI - 1][tplJ + 1] || 0;
+        foundNode[`${tplI - 1}_${tplJ + 1}`] = true;
+        if (northeast !== 0) {
+          returnArr.push({
+            i: tplI--,
+            j: tplJ++,
+          });
+        } else {
+          break;
+        }
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (tplI <= 0 || tplJ <= 0) {
+        break;
+      } else {
+        if (foundNode[`${tplI}_${tplJ}`]) {
+          break;
+        }
+        // foundNode[`${tplI}_${tplJ}`] = true;
+        const northwest = leftDataImage[tplI - 1][tplJ - 1] || 0;
+        foundNode[`${tplI - 1}_${tplJ - 1}`] = true;
+        if (northwest !== 0) {
+          returnArr.push({
+            i: tplI--,
+            j: tplJ--,
+          });
+        } else {
+          break;
+        }
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (tplI >= source1.height - 1 || tplJ >= source1.width - 1) {
+        break;
+      } else {
+        if (foundNode[`${tplI}_${tplJ}`]) {
+          break;
+        }
+        // foundNode[`${tplI}_${tplJ}`] = true;
+        const southeast = leftDataImage[tplI + 1][tplJ + 1] || 0;
+        foundNode[`${tplI + 1}_${tplJ + 1}`] = true;
+        if (southeast !== 0) {
+          returnArr.push({
+            i: tplI++,
+            j: tplJ++,
+          });
+        } else {
+          break;
+        }
+      }
+    }
+
+    tplI = i;
+    tplJ = j;
+    for (;;) {
+      if (tplI >= source1.height - 1 || tplJ <= 0) {
+        break;
+      } else {
+        if (foundNode[`${tplI}_${tplJ}`]) {
+          break;
+        }
+        // foundNode[`${tplI}_${tplJ}`] = true;
+        const southwest = leftDataImage[tplI + 1][tplJ - 1] || 0;
+        foundNode[`${tplI + 1}_${tplJ - 1}`] = true;
+        if (southwest !== 0) {
+          returnArr.push({
+            i: tplI++,
+            j: tplJ--,
+          });
+        } else {
+          break;
+        }
+      }
+    }
+
+    // if (left > 0 && !foundNode[`${i}_${j - 1}`]) {
+    //   const tpl = getBoundaryNodes(i, j - 1) || [];
+    //   returnArr.push({ i, j: j - 1 }, ...tpl);
+    // }
+    // if (right > 0 && !foundNode[`${i}_${j + 1}`]) {
+    //   const tpl = getBoundaryNodes(i, j + 1) || [];
+    //   returnArr.push({ i, j: j + 1 }, ...tpl);
+    // }
+    // if (top > 0 && !foundNode[`${i - 1}_${j}`]) {
+    //   const tpl = getBoundaryNodes(i - 1, j) || [];
+    //   returnArr.push({ i: i - 1, j }, ...tpl);
+    // }
+    // if (bottom > 0 && !foundNode[`${i + 1}_${j}`]) {
+    //   const tpl = getBoundaryNodes(i + 1, j) || [];
+    //   returnArr.push({ i: i + 1, j }, ...tpl);
+    // }
+
+    return returnArr;
+  };
+
+  const different: any[] = [];
+
+  const getBoundary = arr => {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+    arr.forEach(d => {
+      if (d.j < minX) {
+        minX = d.j;
+      }
+      if (d.j > maxX) {
+        maxX = d.j;
+      }
+      if (d.i < minY) {
+        minY = d.i;
+      }
+      if (d.i > maxY) {
+        maxY = d.i;
+      }
+    });
+
+    return {
+      x: minX,
+      y: minY,
+      w: maxX - minX + 1,
+      h: maxY - minY + 1,
+    };
+  };
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (foundNode[`${i}_${j}`]) {
+        continue;
+      }
+      if (leftDataImage[i][j] === 255) {
+        // break;
+        // 找到不同点了
+        const boundaryNodes = getBoundaryNodes(i, j);
+        if (boundaryNodes.length) {
+          const boundary = getBoundary(boundaryNodes);
+          different.push(boundary);
+        }
+      }
+      foundNode[`${i}_${j}`] = true;
+    }
+  }
+
+  // merge nearly boundary
+  const mergeDifferent = nowDifferent => {
+    const len = nowDifferent.length;
+    const newDifferent: any[] = [];
+    if (len <= 1) {
+      return nowDifferent;
+    }
+
+    // 扩展10px
+    const threshold = 100;
+    nowDifferent = nowDifferent.map(c => {
+      return {
+        x: c.x - threshold,
+        y: c.y - threshold,
+        w: c.w + threshold,
+        h: c.h + threshold,
+      };
+    });
+
+    nowDifferent.reduce((total, curr) => {
+      const needMergeNode = total.find(c => checkIsCrossBox(c, curr));
+      if (needMergeNode) {
+        Object.assign(needMergeNode, getMergeBoundary(needMergeNode, curr));
+      } else {
+        total.push(curr);
+      }
+      return total;
+    }, newDifferent);
+
+    // 取消扩展px
+    return newDifferent.map(c => {
+      return {
+        x: c.x + threshold,
+        y: c.y + threshold,
+        w: c.w - threshold,
+        h: c.h - threshold,
+      };
+    });
+  };
+
+  return mergeDifferent(different);
+};
+
+// box
+const checkIsCrossBox = (box1, box2) => {
+  const maxX =
+    box1.x + box1.w >= box2.x + box2.w ? box1.x + box1.w : box2.x + box2.w;
+  const maxY =
+    box1.y + box1.h >= box2.y + box2.h ? box1.y + box1.h : box2.y + box2.h;
+  const minX = box1.x <= box2.x ? box1.x : box2.x;
+  const minY = box1.y <= box2.y ? box1.y : box2.y;
+
+  if (maxX - minX <= box1.w + box2.w && maxY - minY <= box1.h + box2.h) {
+    return true;
+  }
+  return false;
+};
+
+const getMergeBoundary = (box1, box2) => {
+  const maxX =
+    box1.x + box1.w >= box2.x + box2.w ? box1.x + box1.w : box2.x + box2.w;
+  const maxY =
+    box1.y + box1.h >= box2.y + box2.h ? box1.y + box1.h : box2.y + box2.h;
+  const minX = box1.x <= box2.x ? box1.x : box2.x;
+  const minY = box1.y <= box2.y ? box1.y : box2.y;
+
+  const wrapWidth = maxX - minX;
+  const wrapHeight = maxY - minY;
+
+  return {
+    x: minX,
+    y: minY,
+    w: wrapWidth,
+    h: wrapHeight,
+  };
 };
