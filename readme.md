368329e5d5961cfcf5cf62d286b30ca65081dd74
Neil Zeng <neilz@synnex.com>
2020-05-12 10:16:25 +0800

    feat: compare添加合并系数

--- readme.md
+++ readme.md
@@ -53,14 +53,83 @@ Promise.all([imgLoader("./lena.png"), imgLoader("./search.png")]).then(
 );
 ```
 
+
+* 比对图片
+```tsx
+async componentDidMount() {
+  const canvas1 = await loadCanvas('./11.png');
+  const ctx1 = canvas1.getContext('2d');
+  const imageData1 = ctx1!.getImageData(0, 0, canvas1.width, canvas1.height);
+  const rgb1 = getRGBColor(imageData1);
+  const gray1 = rgbToGary(rgb1);
+  const threshold1 = OtsuAlgorithm(gray1);
+  const [newImageData1] = convertBinarization(imageData1, threshold1, true);
+  document.body.appendChild(canvas1);
+
+  const canvas2 = await loadCanvas('./22.png');
+  const ctx2 = canvas2.getContext('2d');
+  const imageData2 = ctx2!.getImageData(0, 0, canvas2.width, canvas2.height);
+  const rgb2 = getRGBColor(imageData2);
+  const gray2 = rgbToGary(rgb2);
+  const threshold2 = OtsuAlgorithm(gray2);
+  const [newImageData2, cropedOffsetX, cropedOffsetY] = convertBinarization(
+    imageData2,
+    threshold2,
+    true,
+  );
+  document.body.appendChild(canvas2);
+
+  const gutter = 100; // 定义合并不同点间隔大小
+  const difference = compareImage(newImageData1, newImageData2, gutter);
+  difference.forEach(d => {
+    canvas2.getContext('2d')!.strokeStyle = 'red';
+    canvas2
+      .getContext('2d')!
+      .strokeRect(d.x + cropedOffsetX, d.y + cropedOffsetY, d.w, d.h);
+  });
+}
+```
+
 ### API
 * index.ts
 ```ts
 declare const imgLoader: (src: string) => Promise<HTMLImageElement>;
-export declare const loadCanvas: (source: string | HTMLImageElement) => Promise<unknown>;
+/**
+ * image加载canvas
+ * @param source string | HTMLImageElement
+ * @param compressWidth 是否压缩 n * n
+ */
+export declare const loadCanvas: (source: string | HTMLImageElement, compressWidth?: number | undefined) => Promise<HTMLCanvasElement>;
 export declare const getImageData: (source: string | HTMLImageElement | HTMLCanvasElement) => Promise<unknown>;
 export declare const getRGBColor: (imageData: ImageData) => number[][];
 export declare const rgbToGary: (rgb: ImageData | number[][]) => number[];
+/**
+ * 大津法取阈值
+ * @param src 灰度值[x1,x2,x3,...]
+ */
+export declare const OtsuAlgorithm: (src?: number[]) => number;
+/**
+ * 转换为二值图像
+ * @param imageData ImageData 图像数据
+ * @param threshold number 阈值 默认125
+ * @param cropBoundary boolean 是否去除边界无效区
+ * @returns 返回 imageData | [imageData, <offsetX>, <offsetY>]
+ */
+export declare const convertBinarization: (imageData: ImageData, threshold?: number, cropBoundary?: boolean) => (number | ImageData)[];
+/**
+ * 转换为中值滤波图像
+ * @param imageData ImageData 图像数据
+ * @param size 滤波大小 默认3 需大于等于3的奇数
+ * @param count 迭代次数
+ */
+export declare const convertMedian: (imageData: ImageData, size?: number, count?: number) => ImageData;
+/**
+ * 转换为均值滤波图像
+ * @param imageData ImageData 图像数据
+ * @param size 滤波大小 默认3 需大于等于3的奇数
+ * @param count 迭代次数
+ */
+export declare const convertAverage: (imageData: ImageData, size?: number, count?: number) => ImageData;
 export default imgLoader;
 ```
 
@@ -76,21 +145,30 @@ export default imgLoader;
  * @param sHeight 搜索图片的height
  * @param type 匹配方式
  */
-type CompareWay =
-  | "CV_TM_SQDIFF"
-  | "CV_TM_SQDIFF_NORMED"
-  | "CV_TM_CCORR"
-  | "CV_TM_CCORR_NORMED"
-  | "CV_TM_CCOEFF"
-  | "CV_TM_CCOEFF_NORMED";
+export declare const getTemplatePos: (template: any, search: any, tWidth: any, tHeight: any, sWidth: any, sHeight: any, type?: "CV_TM_SQDIFF" | "CV_TM_SQDIFF_NORMED" | "CV_TM_CCORR" | "CV_TM_CCORR_NORMED" | "CV_TM_CCOEFF" | "CV_TM_CCOEFF_NORMED" | undefined) => {
+    x: number;
+    y: number;
+};
+/**
+ * 比较两幅图片得到 不同处的坐标
+ * @param source1 ImageData
+ * @param source2 ImageData
+ */
+export declare const compareImage: (source1: ImageData, source2: ImageData) => any;
+```
 
-const getTemplatePos = (
-  template,
-  search,
-  tWidth,
-  tHeight,
-  sWidth,
-  sHeight,
-  type?: CompareWay
-): {x: number; y: number} => {}
+* libg/morphology.js
+```ts
+/**
+ * 膨胀
+ * @param imageData ImageData
+ * @param size default 3 表示 3 * 3的mat, 请传大于1的奇数
+ */
+export declare const dilate: (imageData: ImageData, size?: number) => ImageData;
+/**
+ * 腐蚀
+ * @param imageData ImageData
+ * @param size default 3 表示 3 * 3的mat, 请传大于1的奇数
+ */
+export declare const erode: (imageData: ImageData, size?: number) => ImageData;
 ```
--- src/index.ts
+++ src/index.ts
@@ -17,22 +17,26 @@ const imgLoader = (src: string): Promise<HTMLImageElement> => {
  * @param source string | HTMLImageElement
  * @param compressWidth 是否压缩 n * n
  */
-export const loadCanvas = async (source: string | HTMLImageElement, compressWidth?: number) => {
+export const loadCanvas = async (
+  source: string | HTMLImageElement,
+  compressWidth?: number,
+): Promise<HTMLCanvasElement> => {
   let imgTarget = source;
   const handleDrawImage = (target: HTMLImageElement) => {
-    const canvasEle = document.createElement("canvas");
-    const canvasCtx = canvasEle.getContext("2d");
+    const canvasEle = document.createElement('canvas');
+    const canvasCtx = canvasEle.getContext('2d');
 
     canvasEle.width = compressWidth || target.width;
     canvasEle.height = compressWidth || target.height;
 
+    // eslint-disable-next-line no-unused-expressions
     canvasCtx?.drawImage(target, 0, 0, canvasEle.width, canvasEle.height);
 
     return canvasEle;
   };
   return new Promise((resolve, reject) => {
     try {
-      if (typeof source === "string") {
+      if (typeof source === 'string') {
         imgLoader(source).then(res => {
           imgTarget = res;
           const canvasEle = handleDrawImage(imgTarget);
@@ -40,7 +44,7 @@ export const loadCanvas = async (source: string | HTMLImageElement, compressWidt
         });
       } else {
         if (!(imgTarget instanceof HTMLImageElement)) {
-          throw new Error("loadCanvas error");
+          throw new Error('loadCanvas error');
         }
         const canvasEle = handleDrawImage(imgTarget);
         resolve(canvasEle);
@@ -52,12 +56,12 @@ export const loadCanvas = async (source: string | HTMLImageElement, compressWidt
 };
 
 export const getImageData = (
-  source: string | HTMLImageElement | HTMLCanvasElement
+  source: string | HTMLImageElement | HTMLCanvasElement,
 ) => {
   if (source instanceof HTMLCanvasElement) {
     return new Promise(resolve => {
       const imageData = source
-        .getContext("2d")
+        .getContext('2d')
         ?.getImageData(0, 0, source.width, source.height);
       resolve(imageData);
     });
@@ -65,7 +69,7 @@ export const getImageData = (
   return new Promise(resolve => {
     loadCanvas(source).then((canvas: HTMLCanvasElement) => {
       const imageData = canvas
-        .getContext("2d")
+        .getContext('2d')
         ?.getImageData(0, 0, canvas.width, canvas.height);
       resolve(imageData);
     });
@@ -96,7 +100,7 @@ export const rgbToGary = (rgb: number[][] | ImageData) => {
   return validRgb.map(color => {
     return parseInt(
       String((299 * color[0] + 587 * color[1] + 114 * color[2]) / 1000),
-      10
+      10,
     );
   });
 };
@@ -142,7 +146,7 @@ export const OtsuAlgorithm = (src: number[] = []) => {
     sumB += t * histData[t];
 
     const aB = sumB / wB; // 当前背景灰度值平均值
-    const aF = (totalGraySum - sumB) / wF;  // 当前前景灰度值平均值
+    const aF = (totalGraySum - sumB) / wF; // 当前前景灰度值平均值
 
     // 类间平方差
     const varTemp = wB * wF * (aB - aF) ** 2;
@@ -160,14 +164,23 @@ export const OtsuAlgorithm = (src: number[] = []) => {
  * @param imageData ImageData 图像数据
  * @param threshold number 阈值 默认125
  * @param cropBoundary boolean 是否去除边界无效区
+ * @returns 返回 imageData | [imageData, <offsetX>, <offsetY>]
  */
-export const convertBinarization = (imageData: ImageData, threshold = 125, cropBoundary = false) => {
+export const convertBinarization = (
+  imageData: ImageData,
+  threshold = 125,
+  cropBoundary = false,
+) => {
   for (let i = 0; i < imageData.data.length; i += 4) {
-
     // 先转灰度值
     const grayColor = parseInt(
-      String((299 * imageData.data[i] + 587 * imageData.data[i + 1] + 114 * imageData.data[i + 2]) / 1000),
-      10
+      String(
+        (299 * imageData.data[i] +
+          587 * imageData.data[i + 1] +
+          114 * imageData.data[i + 2]) /
+          1000,
+      ),
+      10,
     );
     const shouldValue = grayColor >= threshold ? 255 : 0;
     imageData.data[i] = shouldValue;
@@ -176,7 +189,7 @@ export const convertBinarization = (imageData: ImageData, threshold = 125, cropB
   }
 
   if (!cropBoundary) {
-    return imageData;
+    return [imageData];
   }
 
   const { width, height } = imageData;
@@ -190,6 +203,7 @@ export const convertBinarization = (imageData: ImageData, threshold = 125, cropB
     for (let j = 0; j < width; j++) {
       const offset = (i * width + j) * 4;
       if (imageData.data[offset] === 0) {
+        // debugger;
         if (i < startY) {
           startY = i;
         }
@@ -209,13 +223,14 @@ export const convertBinarization = (imageData: ImageData, threshold = 125, cropB
   const cropedWidth = endX - startX + 1;
   const cropedHeight = endY - startY + 1;
 
+  // debugger;
+
   const newImageData = new ImageData(cropedWidth, cropedHeight);
 
   for (let j = startY, shouldM = 0; j <= endY; j++, shouldM++) {
     for (let i = startX, shouldN = 0; i <= endX; i++, shouldN++) {
-
       const offset = (j * width + i) * 4;
-      const realOffset = (shouldM * width + shouldN) * 4;
+      const realOffset = (shouldM * cropedWidth + shouldN) * 4;
       const pxValue = imageData.data[offset];
       const pxValueAlpha = imageData.data[offset + 3];
       newImageData.data[realOffset] = pxValue;
@@ -225,8 +240,8 @@ export const convertBinarization = (imageData: ImageData, threshold = 125, cropB
     }
   }
 
-  return newImageData;
-}
+  return [newImageData, startX, startY];
+};
 
 /**
  * 转换为中值滤波图像
@@ -235,11 +250,10 @@ export const convertBinarization = (imageData: ImageData, threshold = 125, cropB
  * @param count 迭代次数
  */
 export const convertMedian = (imageData: ImageData, size = 3, count = 1) => {
-
   const { width: tWidth, height: tHeight } = imageData;
   const sWidth = size;
   const sHeight = size;
-  const convert = (imageData: ImageData, size = 3) => {
+  const convert = (curr: ImageData, size = 3) => {
     for (let th = 0; th < tHeight; th += 1) {
       for (let tW = 0; tW < tWidth; tW += 1) {
         if (tW + sWidth > tWidth || th + sHeight > tHeight) {
@@ -252,9 +266,9 @@ export const convertMedian = (imageData: ImageData, size = 3, count = 1) => {
         for (let sH = 0; sH < sHeight; sH += 1) {
           for (let sW = 0; sW < sWidth; sW += 1) {
             const tempPix = ((th + sH) * tWidth + tW + sW) * 4;
-            const tValueR = imageData.data[tempPix];
-            const tValueG = imageData.data[tempPix + 1];
-            const tValueB = imageData.data[tempPix + 2];
+            const tValueR = curr.data[tempPix];
+            const tValueG = curr.data[tempPix + 1];
+            const tValueB = curr.data[tempPix + 2];
 
             tempR.push(tValueR);
             tempG.push(tValueG);
@@ -265,9 +279,9 @@ export const convertMedian = (imageData: ImageData, size = 3, count = 1) => {
         // 获取中值索引
         const index = ~~(size / 2);
 
-        imageData.data[currPixIndex] = tempR.sort()[index];
-        imageData.data[currPixIndex + 1] = tempG.sort()[index];
-        imageData.data[currPixIndex + 2] = tempB.sort()[index];
+        curr.data[currPixIndex] = tempR.sort()[index];
+        curr.data[currPixIndex + 1] = tempG.sort()[index];
+        curr.data[currPixIndex + 2] = tempB.sort()[index];
       }
     }
   };
@@ -277,7 +291,7 @@ export const convertMedian = (imageData: ImageData, size = 3, count = 1) => {
   }
 
   return imageData;
-}
+};
 
 /**
  * 转换为均值滤波图像
@@ -286,12 +300,11 @@ export const convertMedian = (imageData: ImageData, size = 3, count = 1) => {
  * @param count 迭代次数
  */
 export const convertAverage = (imageData: ImageData, size = 3, count = 1) => {
-
   const { width: tWidth, height: tHeight } = imageData;
   const sWidth = size;
   const sHeight = size;
 
-  const convert = (imageData: ImageData, size = 3) => {
+  const convert = (curr: ImageData, size = 3) => {
     for (let th = 0; th < tHeight; th += 1) {
       for (let tW = 0; tW < tWidth; tW += 1) {
         if (tW + sWidth > tWidth || th + sHeight > tHeight) {
@@ -304,9 +317,9 @@ export const convertAverage = (imageData: ImageData, size = 3, count = 1) => {
         for (let sH = 0; sH < sHeight; sH += 1) {
           for (let sW = 0; sW < sWidth; sW += 1) {
             const tempPix = ((th + sH) * tWidth + tW + sW) * 4;
-            const tValueR = imageData.data[tempPix];
-            const tValueG = imageData.data[tempPix + 1];
-            const tValueB = imageData.data[tempPix + 2];
+            const tValueR = curr.data[tempPix];
+            const tValueG = curr.data[tempPix + 1];
+            const tValueB = curr.data[tempPix + 2];
 
             tempRSum += tValueR;
             tempGSum += tValueG;
@@ -319,9 +332,9 @@ export const convertAverage = (imageData: ImageData, size = 3, count = 1) => {
         const averageG = Math.ceil(tempGSum / total);
         const averageB = Math.ceil(tempBSum / total);
 
-        imageData.data[currPixIndex] = averageR;
-        imageData.data[currPixIndex + 1] = averageG;
-        imageData.data[currPixIndex + 2] = averageB;
+        curr.data[currPixIndex] = averageR;
+        curr.data[currPixIndex + 1] = averageG;
+        curr.data[currPixIndex + 2] = averageB;
       }
     }
   };
@@ -331,6 +344,6 @@ export const convertAverage = (imageData: ImageData, size = 3, count = 1) => {
   }
 
   return imageData;
-}
+};
 
 export default imgLoader;
